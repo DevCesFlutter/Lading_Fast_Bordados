242dd62faaa2ba4bb5630604d3c0f158bf1d8283
DevCesFlutter <cesarnegretes100@gmail.com>
2025-05-03 13:28:58 -0400

    correccion de redireccion en el formulariohttps://fastbordados.vercel.app/

--- src/components/formulario.tsx
+++ src/components/formulario.tsx
@@ -12,7 +12,11 @@ export default function ContactForm() {
           method="POST"
         >
           <input type="hidden" name="_captcha" value="false" />
-          <input type="hidden" name="_next" value="http://localhost:3000/" />
+          <input
+            type="hidden"
+            name="_next"
+            value="https://fastbordados.vercel.app/"
+          />
 
           <input
             name="name"
