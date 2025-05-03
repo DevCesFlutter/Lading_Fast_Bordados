7a80b3cc605c0870c62cf6fe02b6c5132c1ae6e2
DevCesFlutter <cesarnegretes100@gmail.com>
2025-05-03 00:38:42 -0400

    correjimos la imagen error de mayuscula

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
         {/* TItulo */}
         <div>
           <img
-            src="Logo.png"
+            src="logo.png"
             alt="Muestra de bordado"
             className=" w-30 h-30  mx-auto mt-8 rounded-4xl"
           />
