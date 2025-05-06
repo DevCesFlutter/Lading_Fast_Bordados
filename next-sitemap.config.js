4ac61921435d9de39a643e10a514aa93f279a9a6
DevCesFlutter <cesarnegretes100@gmail.com>
2025-05-06 01:06:59 -0400

    robots y sitemap nuevos

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://fastbordados.vercel.app", // ← Reemplaza esto por tu dominio real
+  generateRobotsTxt: true, // Genera también el archivo robots.txt
+};
